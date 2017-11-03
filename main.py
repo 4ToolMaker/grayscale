33e08e455886adb7c1133865ebb8b100dcd72ca4
Paulo Lopez <8420448+4ToolMaker@users.noreply.github.com>
2017-11-03 08:40:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Grayscale', gray_image)
 cv2.waitKey(3)
 cv2.destroyAllWindows()
+
+# printing out the dimensions of the image
+# entering an array and it contains a tuple
+# Two dimensions of height and widths
+# the third column represent the rgb values of the image
+
+print(image.shape)
+
+# saving images 
+
+#cv2.imwrite('output.jpg', image)
+#cv2.imwrite('output.png', image)
+cv2.imwrite('gray.jpg', gray_image)
+
